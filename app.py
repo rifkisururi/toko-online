649935fb31cc52ebf44932b824024f085808ca59
rifki ahmad sururi <rifkiahmadsururi@outlook.com>
2024-11-18 07:31:39 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(200), nullable=False)
+
+# Inisialisasi database
+with app.app_context():
+    db.create_all()
+
+# Home (Read)
+@app.route('/')
+def index():
+    items = Item.query.all()
+    return render_template('index.html', items=items)
+
+# Create
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        new_item = Item(name=name, description=description)
+        db.session.add(new_item)
+        db.session.commit()
+        return redirect('/')
+    return render_template('create.html')
+
+# Update
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    item = Item.query.get_or_404(id)
+    if request.method == 'POST':
+        item.name = request.form['name']
+        item.description = request.form['description']
+        db.session.commit()
+        return redirect('/')
+    return render_template('update.html', item=item)
+
+# Delete
+@app.route('/delete/<int:id>')
+def delete(id):
+    item = Item.query.get_or_404(id)
+    db.session.delete(item)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
